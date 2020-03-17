075161e2e7940421b2054580bd9316cbc2145b50
Kaique Souza <kaique16souza@hotmail.com>
2020-03-17 13:10:23 -0300

    Inserir Git

---
+++ Projeto_Marvel/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Projeto_Marvel.Models;
+
+namespace Projeto_Marvel.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Projeto_QuadrinhoEntities1 db = new Projeto_QuadrinhoEntities1();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult QmSomos()
+        {
+            return View();
+        }
+
+        public ActionResult Listar()
+        {
+            return View(db.FALE.ToList());
+        }
+
+        public ActionResult Contato(FALE fale)
+        {
+            if (ModelState.IsValid)
+            {
+                db.FALE.Add(fale);
+                db.SaveChanges();
+
+                return RedirectToAction("Listar");
+            }
+
+            else
+            {
+                return View("Contato");
+            }
+        }
+
+        public ActionResult ComoChegar()
+        {
+            return View();
+        }
+
+        public ActionResult Eventos()
+        {
+            return View();
+        }
+    }
+}
---
+++ Projeto_Marvel/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Projeto_Marvel.Models;
+
+namespace Projeto_Marvel.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index(LOGIN login)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new Projeto_QuadrinhoEntities1())
+                {
+                    var v = db.LOGIN.Where(a => a.USUARIO.Equals(login.USUARIO) && a.SENHA.Equals(login.SENHA)).FirstOrDefault();
+                    if (v != null)
+                    {
+                        return RedirectToAction("Eventos", "Home");
+                    }
+                    else
+                    {
+                        ViewBag.alerta = "<script>" + "alert('Área reservada para pessoas cadastradas');" + "</script>";
+                    }
+
+                }
+            }
+            return View(login);
+        }
+    }
+}
